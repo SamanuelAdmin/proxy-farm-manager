4e228385fd0cf502f83dbaaae94aacc6c01cfe35
SamanuelAdmin <samolenkosvatik93@gmail.com>
2025-12-08 02:06:23 +0200

    isOnline checker (device.py)

--- pfmanager/device/device.py
+++ pfmanager/device/device.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import adbutils
 
 from .idevice import IDevice
@@ -6,8 +8,18 @@ from .idevice import IDevice
 class Device(IDevice):
     def __init__(self, serial: str, adbDevice: adbutils.AdbDevice):
         self._serial = serial
-        self.__adb: adb.AdbDevice = adbDevice
+        self.__adb: adbutils.AdbDevice = adbDevice
 
+    def ifOnline(function: Callable) -> Callable:
+        def wrapper(self, *args, **kwargs):
+            if not self.__adb.isOnline():
+                raise Exception("Device is not online")
+
+            return function(self, *args, **kwargs)
+
+        return wrapper
+
+    @ifOnline
     def checkConnection(self, timeout: int=1000) -> int:
         commandResult: str = self.__adb.shell(f'ping -c 1 -W {timeout} example.com')
         if 'Network is unreachable' in commandResult: return 0
